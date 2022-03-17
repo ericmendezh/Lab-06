e4b03c45cefa8674481ebbcffe861a10bba60f5b
Mendez <eric.mendez@siemens-energy.com>
2022-03-16 21:35:30 -0600

    Added option to substract two numbers

--- Lab06.java
+++ Lab06.java
@@ -6,12 +6,28 @@ public class Lab06 {
         Scanner input = new Scanner(System.in);
         double firstNumber;
         double secondNumber;
+        int menuSelection;
 
-        System.out.print("Enter first number: ");
-        firstNumber = input.nextDouble();
-        System.out.print("Enter second number: ");
-        secondNumber = input.nextDouble();
-        System.out.println("The addition of both numbers is " + (firstNumber + secondNumber));
 
+        System.out.println("1. Add two numbers");
+        System.out.println("2. Subtract two numbers");
+        System.out.print("Please make a selection ");
+        menuSelection = input.nextInt();
+
+        if( menuSelection == 1) {
+            System.out.print("Enter first number: ");
+            firstNumber = input.nextDouble();
+            System.out.print("Enter second number: ");
+            secondNumber = input.nextDouble();
+            System.out.println("The addition of both numbers is " + (firstNumber + secondNumber));
+        }
+        if (menuSelection == 2)
+        {
+            System.out.print("Enter first number: ");
+            firstNumber = input.nextDouble();
+            System.out.print("Enter second number: ");
+            secondNumber = input.nextDouble();
+            System.out.println("The subtraction of both numbers is " + (firstNumber - secondNumber));
+        }
     }
 }
